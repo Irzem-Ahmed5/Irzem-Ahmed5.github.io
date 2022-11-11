bfa95216dde035fa40b53362bda10750fcec6d35
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2022-11-11 10:28:41 -0500

    finished arrays.html

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,18 +1,59 @@
 var persons = ["Irzem", "Manzoor", "Zakir"];
 var salaries = [65, 100, 100];
 
+function addSalary() {
 
-// window.onload = function () {
-//     document.getElementById("display-results").onclick = displayResults;
-//     document.getElementById("add-salary").onclick = addSalary;
-//     document.getElementById("display-salary").onclick = displaySalary;
-//     document.getElementById("persons").focus();
-// };
+    let nameInput = document.getElementById('persons').value;
+    let salaryInput = document.getElementById('salary').value;
 
-function doSomething() {
-    prompt("Hello?");
+    if (salaryInput < 0 || salaryInput > 300 || salaryInput == "") {
+        alert("Incorrect salary input. Please enter a value within 0-300");
+        salaryInput = 0;
+    } else {
+        salaryInput = parseInt(salaryInput);
+        if (persons.includes(nameInput))
+            salary[persons.indexOf(nameInput)] = salaryInput;
+    }
+
+    alert("Salary added!")
+
+    //tester
+    console.log("Person: " + nameInput + " Sal: " + salary[persons.indexOf(nameInput)])
+
 }
 
+function displayResults() {
+    let sum = 0;
+    let average = 0;
+
+    for (i = 0; i < salaries.length - 1; i++) {
+        sum += salaries[i];
+    }
+
+    average = (sum / 3);
+
+    document.getElementById('average-result').innerHTML = "Average salary is " + average + "k";
+
+    //tester
+    console.log(average);
+
+}
+
+function displaySalary() {
+    let html = "";
+
+    html += "<tr><th>Name</th> <th>Salary (in thousands of dollars)</th></tr>";
+
+    for (i = 0; i < salaries.length; i++) {
+        html += "<tr><td>" + persons[i] + "</td><td>" + salaries[i] + "</td></tr>";
+
+    }
+    html += "</table>";
+
+    document.getElementById('table').innerHTML = "<h2>Salaries</h2>" + html;
+
+
+}
 
 
 

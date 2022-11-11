0aeaf45f10b356020358b599e33ec6b086913cd5
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2022-11-11 10:56:45 -0500

    updated arrays.html

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,10 +1,13 @@
 var persons = ["Irzem", "Manzoor", "Zakir"];
-var salaries = [65, 100, 100];
+var salaries = [65, 101, 100];
+
+
+
 
 function addSalary() {
+    var nameInput = document.getElementById('persons').value;
+    var salaryInput = document.getElementById('salary').value;
 
-    let nameInput = document.getElementById('persons').value;
-    let salaryInput = document.getElementById('salary').value;
 
     if (salaryInput < 0 || salaryInput > 300 || salaryInput == "") {
         alert("Incorrect salary input. Please enter a value within 0-300");
@@ -12,19 +15,22 @@ function addSalary() {
     } else {
         salaryInput = parseInt(salaryInput);
         if (persons.includes(nameInput))
-            salary[persons.indexOf(nameInput)] = salaryInput;
+            salaries[persons.indexOf(nameInput)] = salaryInput;
+        alert("Salary added!")
     }
 
-    alert("Salary added!")
+
 
     //tester
-    console.log("Person: " + nameInput + " Sal: " + salary[persons.indexOf(nameInput)])
-
+    console.log("Person: " + nameInput + " Sal: " + salaries[persons.indexOf(nameInput)])
+    console.log("salInput" + salaryInput)
 }
 
 function displayResults() {
     let sum = 0;
     let average = 0;
+    let highestIndex = 0;
+    let highestSalary = 0;
 
     for (i = 0; i < salaries.length - 1; i++) {
         sum += salaries[i];
@@ -32,10 +38,18 @@ function displayResults() {
 
     average = (sum / 3);
 
-    document.getElementById('average-result').innerHTML = "Average salary is " + average + "k";
+    for (i = 0; i < salaries.length - 1; i++) {
+        if (salaries[i] > highestSalary) {
+            highestIndex = i;
+            highestSalary = salaries[i];
+        }
+    }
+
+    document.getElementById('average-result').innerHTML = "Average salary is " + average + "k " +
+        "and highest salary is for " + "Person: " + persons[highestIndex] + " Salary: " + salaries[highestIndex];
 
     //tester
-    console.log(average);
+    //console.log(average);
 
 }
 

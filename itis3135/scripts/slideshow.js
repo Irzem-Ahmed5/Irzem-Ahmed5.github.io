093f9b5bc7027fecb4aaa78e2c9f9c9a8868c5de
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2022-11-28 18:49:38 -0500

    tentative slideshow finish

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,17 +1,40 @@
 console.log("welcome");
-var pictures = ["#slide-pic-1", "#slide-pic-2", "#slide-pic-3", "#slide-pic-4", "#slide-pic-5"];
+var pictures = ["#slide-pic-1", "#slide-pic-2", "#slide-pic-3", "#slide-pic-4", "#slide-pic-5", "slide-pic-6"];
+var currentPictureIndex = 0;
+
 
 $(document).ready(function () {
 
     $('.slides').hide();
-    $('#slide-pic-1').show()
+    $('#slide-pic-1').show();
+
 
 });
 
 function nextButton() {
-    for (let i = 0; i < 5; i++) {
-        $(pictures[i]).show(500);
-        $(pictures[i - 1]).hide(250);
+    currentPictureIndex++;
+
+    if (currentPictureIndex == 6) {
+        $('#slide-pic-6').hide(pictures[5])
+        currentPictureIndex = -1
     }
 
+    if (currentPictureIndex == 5) {
+        $('#slide-pic-6').show(pictures[5])
+    }
+
+    $(pictures[currentPictureIndex]).show(500);
+    $(pictures[currentPictureIndex - 1]).hide();
+    console.log("curr pic ind" + currentPictureIndex)
+}
+
+function previousButton() {
+    $(pictures[currentPictureIndex - 1]).show(500);
+    $(pictures[currentPictureIndex]).hide();
+
+    if (currentPictureIndex < 1) {
+        currentPictureIndex = 5;
+    }
+
+    currentPictureIndex -= 1;
 }

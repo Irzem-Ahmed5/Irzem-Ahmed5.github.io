09ef2e11f30b51d2293886198521b1cd74d14dfe
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2022-12-10 21:52:42 -0500

    finish activity 10

---
+++ itis3135/scripts/activity10.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('.bunny-pic').hide();
+
+
+});
+
+function showPic(id) {
+
+    hideAll();
+    $(id).fadeIn();
+
+}
+
+function hideAll() {
+    $('.bunny-pic').hide();
+}

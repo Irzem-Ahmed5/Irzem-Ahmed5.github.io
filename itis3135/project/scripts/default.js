19fe701da274252ef47912592cc730e70b78345d
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2022-12-14 21:12:39 -0500

    fix videos

---
+++ itis3135/project/scripts/default.js
@@ -0,0 +1,6 @@
+$("#button1").click(function () {
+    $("#history-info").load("history-content.html #ancient");
+});
+$("#button2").click(function () {
+    $("#history-info").load("history-content.html #middle");
+});
